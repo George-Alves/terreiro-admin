2bd072c6bda40670f43fb2086632a7eefccf8747
George Alves <railanabreu@MacBook-Pro-de-George.local>
2025-02-16 22:28:29 -0300

    requeriments

--- gestao_terreiro/settings.py
+++ gestao_terreiro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i)=@grseag_0$a%tcr_6!r3ni1#w%bh6v!233uxhdx)&3)ucv_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
